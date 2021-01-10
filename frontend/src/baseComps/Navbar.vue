a6c5c6c50496d231571183adbcb6b66c488049ed
Qiling <professorqtaku@gmail.com>
2021-01-10 17:13:00 +0100

    added links in navbar

--- frontend/src/baseComps/Navbar.vue
+++ frontend/src/baseComps/Navbar.vue
@@ -1,16 +1,19 @@
 <template>
   <nav class="nav-wrapper">
-    <div class="container">
-      <a href="#" data-target="mobile-demo" class="sidenav-trigger">
+    <a href="#" data-target="mobile-demo" class="sidenav-trigger">
         <i class="material-icons">menu</i>
       </a>
-      <router-link to="/" class="brand-logo">Filmvisarna</router-link>
+    <router-link to="/" class="brand-logo">Filmvisarna</router-link>
+    <div class="container">
       <ul class="hide-on-med-and-down">
-
         <li><router-link to="/">Start</router-link></li>
         <li><router-link to="/movies">Filmer</router-link></li>
         <li><router-link to="/info">Biljett info</router-link></li>
         <li><router-link to="/about">Om oss</router-link></li>
+      </ul>
+      <ul class="hide-on-med-and-down right">
+        <li><router-link to="/">Bli medlem</router-link></li>
+        <li><router-link to="/">{{loggedIn}}</router-link></li>
       </ul>
     </div>
   </nav>
@@ -20,6 +23,8 @@
     <li><router-link to="/movies">Filmer</router-link></li>
     <li><router-link to="/info">Biljett info</router-link></li>
     <li><router-link to="/about">Om oss</router-link></li>
+    <li><router-link to="/">Bli medlem</router-link></li>
+    <li><router-link to="/">{{loggedIn}}</router-link></li>
   </ul>
 
 </template>
@@ -27,6 +32,19 @@
 <script>
 import M from 'materialize-css'
 export default {
+  data(){
+    return{
+      isLoggedIn: false
+    }
+  },
+  computed:{
+    loggedIn(){
+      if(this.isLoggedIn){
+        return 'Logga ut'
+      }
+      return 'Logga in'
+    }
+  },
   mounted(){
     M.AutoInit();
   }
@@ -36,6 +54,5 @@ export default {
 <style scoped>
   nav{
     background-color: var(--purpleish);
-    text-align: center;
   }
 </style>
