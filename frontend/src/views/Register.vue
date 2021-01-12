9e919e592e8397d719d416f9118e988e58056da0
oscargregorius <oscar.gregorius@gmail.com>
2021-01-12 15:46:00 +0100

    Some small adjustments // Oscar & Ali

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -3,13 +3,13 @@
     <form v-on:submit.prevent="becomeMember">
       <h3>Bli medlem</h3>
       <label>Namn</label>
-      <input v-model="name" required type="text">
+      <input v-model="name" required type="text" />
       <label>Efternamn</label>
-      <input v-model="lastName" required type="text">
+      <input v-model="lastName" required type="text" />
       <label>Email</label>
-      <input v-model="email" required type="email">
+      <input v-model="email" required type="email" />
       <label>Lösenord</label>
-      <input v-model="password" required type="password">
+      <input v-model="password" required type="password" />
       <p v-if="alreadyMember">Emailen är upptagen..</p>
       <button>Bli medlem</button>
     </form>
@@ -18,63 +18,69 @@
 
 <script>
 export default {
-data(){
-  return{
-    name: '',
-    lastName: '',
-    email: '',
-    password: '',
-    alreadyMember: false   
-  }
-},  
-methods:{
-  becomeMember(){
-    let user = 
-    {
-      name: this.name,
-      lastName: this.lastName,
-      email: this.email,
-      password: this.password
-    }
-    if(!this.users.length){
-      this.$store.dispatch('addUser', user)
-    }else{
-      for(let i = 0; i < this.users.length; i++){
-        if(this.users[i].email === user.email){
-          this.alreadyMember = true;
+  data() {
+    return {
+      name: "",
+      lastName: "",
+      email: "",
+      password: "",
+      alreadyMember: false,
+    };
+  },
+  methods: {
+    becomeMember() {
+      let user = {
+        name: this.name,
+        lastName: this.lastName,
+        email: this.email,
+        password: this.password,
+      };
+      if (!this.users.length) {
+        this.$store.dispatch("addUser", user);
+        this.$router.push("/login");
+      } else {
+        for (let i = 0; i < this.users.length; i++) {
+          if (this.users[i].email === user.email) {
+            this.alreadyMember = true;
+          }else{
+            this.alreadyMember = false;
+          }
+        }
+        if (!this.alreadyMember) {
+          this.$store.dispatch("addUser", user);
+          this.$router.push("/login");
         }
       }
-      if(!this.alreadyMember){
-        this.$store.dispatch('addUser', user)
-      }
-    }
-  }
-},
-computed:{
-  users(){
-  return this.$store.state.user
-  }
-}
-}
+    },
+  },
+  computed: {
+    users() {
+      return this.$store.state.user;
+    },
+  },
+};
 </script>
 
 <style scoped>
-h3{
+h3 {
   margin: 0 0 10px 0;
   color: white;
 }
-.regrister{
+.regrister {
   max-width: 40%;
-  background:  rgba(49, 45, 45, 0.664);
+  background: rgba(49, 45, 45, 0.664);
   margin: 50px auto;
   padding: 20px;
   border-radius: 10px;
 }
-label{
+label {
   color: white;
   text-align: center;
 }
-button{
+input {
+  color: white;
+}
+button {
   cursor: pointer;
   display: block;
   margin: 20px auto;
@@ -85,10 +91,10 @@ button{
   opacity: 75%;
   color: white;
 }
-button:hover{
+button:hover {
   opacity: 100%;
 }
-p{
+p {
   color: red;
 }
 </style>
