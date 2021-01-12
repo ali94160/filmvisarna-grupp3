1509a31052d14f0a8d63fde01ebd6001a009bb3a
postila <isabella.postila@hotmail.com>
2021-01-12 11:35:11 +0100

    Render movie description, title and poster
    Co-Authored-By: lukasbloom93 <72065185+lukasbloom93@users.noreply.github.com>

--- frontend/src/views/MovieDetails.vue
+++ frontend/src/views/MovieDetails.vue
@@ -1,21 +1,45 @@
 <template>
-  <h1>Movie Detail Page</h1>
-  <h3>{{movie.title}}</h3>
+<div class="container">
+<div class="moviePoster">
+  <img :src="movie.imgUrl" :alt="movie.title" />
+</div>
+  <div class="movieInfo">
+    <h3>{{ movie.title }}</h3>
+    <p>{{ movie.description }}</p>
+    </div>
+    </div>
 </template>
 
 <script>
 export default {
-computed: {
-  id() {
-    return this.$route.params.id
-  },
-      movie() {
+  computed: {
+    id() {
+      return this.$route.params.id;
+    },
+    movie() {
       return this.$store.state.movies.filter((movie) => movie.id == this.id)[0];
     },
-}
-}
+  },
+};
 </script>
 
 <style scoped>
+.container{
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 0 auto;
+  margin-top: 25px;
+  background-color: #333;
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 15px 1px rgb(61, 61, 61);
+}
+.moviePoster img {
+  display: grid;
+  max-width: 400px;
 
+}
+.movieInfo{
+  display: grid;
+}
 </style>
