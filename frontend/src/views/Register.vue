b9f9418d8f0c1ee3c175aeee0311f8366aaf53db
oscargregorius <oscar.gregorius@gmail.com>
2021-01-12 10:41:05 +0100

    Created the regrister shell.

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,5 +1,18 @@
 <template>
-  <h1>Bli medlem</h1>
+  <div class="regrister">
+    <form>
+      <h3>Bli medlem</h3>
+      <label>Namn</label>
+      <input type="text">
+      <label>Efternamn</label>
+      <input type="text">
+      <label>Email</label>
+      <input type="email">
+      <label>Lösenord</label>
+      <input type="password">
+      <button>Bli medlem</button>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -8,6 +21,18 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+h3{
+  margin: 0 0 10px 0;
+  color: black;
+}
+.regrister{
+  max-width: 40%;
+  background: white;
+  margin: 50px auto;
+  padding: 20px;
+}
+label{
+  text-align: center;
+}
 </style>
