faf780760125f6894f7b5642ec832d21ebe05820
postila <isabella.postila@hotmail.com>
2021-01-12 10:59:52 +0100

    Created movieDetailView
    Co-Authored-By: lukasbloom93 <72065185+lukasbloom93@users.noreply.github.com>

---
+++ frontend/src/views/MovieDetails.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Movie Detail Page</h1>
+  <h3>{{movie.title}}</h3>
+</template>
+
+<script>
+export default {
+computed: {
+  id() {
+    return this.$route.params.id
+  },
+      movie() {
+      return this.$store.state.movies.filter((movie) => movie.id == this.id)[0];
+    },
+}
+}
+</script>
+
+<style scoped>
+
+</style>
