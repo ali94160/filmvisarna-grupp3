9f78c11b05933b9ddd2089963f63297864aa0e71
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-01-12 12:40:54 +0100

    Made booking vue

---
+++ frontend/src/views/Booking.vue
@@ -0,0 +1,67 @@
+<template>
+  <h1>Booking Page</h1>
+  <div class="container">
+    <div class="salon">
+
+    </div>
+
+
+      <div class="info">
+        <span>Platser:</span>
+          <input type="number" value="chosenSeats" v-model="chosenSeats"/>  
+
+
+
+    </div>
+  </div>
+</template>
+
+<script>
+import BookingInput from '../components/BookingInput.vue'
+export default {
+  
+data(){
+  return{
+  chosenSeats:2
+  }
+},
+
+components:{
+  BookingInput
+}
+
+}
+</script>
+
+<style scoped>
+.container input{
+  background-color: aliceblue !important;
+  max-width: 10vh;
+  text-align: center;
+}
+
+.container{
+  display: grid;
+  grid-template-columns: repeat(2, 3fr);
+  grid-gap: 60px;
+  background-color: burlywood;
+  padding: 30px;
+ 
+}
+
+span{
+  padding: 5px;
+  color: black;
+
+}
+
+.salon{
+  background-color: brown;
+}
+
+.info{
+  background-color: pink;
+  padding: 10px;
+}
+
+</style>
