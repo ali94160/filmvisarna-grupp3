8d2de139356524fb25bef8f4feb11ffdcf584dcb
Ali94160 <ali.al-mousawi@hotmail.com>
2021-01-12 11:34:56 +0100

    updated: version 1.2
    Login page is now completed.
    If email or passsword is invalid show a message thats its not correct.
    Also added more styling and fixed positions.

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,41 +1,61 @@
 <template>
-  <h3>Logga in</h3>
-
   <form v-on:submit.prevent>
+    <h3>Logga in</h3>
+
     <label>Epost:</label>
-    <input type="email" />
+    <input required v-model="email" type="email" />
     <label>Lösenord:</label>
-    <input type="password" />
+    <input required v-model="password" type="password" />
     <button>Logga in</button>
+    <button>Registrera</button>
+    <p class="failedLogin" v-if="true">Ej giltigt Epost/lösenord</p>
   </form>
 
   <div class="loginFeatures">
     <h4>Varför ska jag logga in?</h4>
     <ul>
       <li>Boka biljetter snabbt och säkert.</li>
+      <li>Boka en eller flera platser.</li>
       <li>Få tillgång till dina kommande och använda biljetter.</li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      show: false,
+    };
+  },
+
+  methods: {},
+};
 </script>
 
 <style scoped>
 h3 {
+  font-size: 30px;
   text-align: center;
-  margin-bottom: 50px;
+}
+label {
+  font-size: 15px;
 }
 
 form {
-  margin: 0 auto;
+  padding: 1px 15px 1px 15px;
+  background: rgba(49, 45, 45, 0.664);
+  margin: 30px auto;
   border-radius: 10px;
   max-width: 300px;
+  margin-bottom: 50px;
 }
 
 form > button {
-  background-color: #4caf50; /* Green */
+  margin: 15px 5px 5px 0px;
+  background-color: #4caf50;
   border: none;
   color: white;
   padding: 15px 32px;
@@ -57,6 +77,7 @@ form > input {
 }
 
 .loginFeatures {
+  padding: 10px;
   max-width: 500px;
   margin: 0 auto;
   background: rgba(49, 45, 45, 0.664);
@@ -72,6 +93,19 @@ li::before {
   margin-right: 8px;
   color: red;
   font-size: 20px;
+  font-weight: bolder;
+}
+
+.failedLogin {
+  text-align: center;
+  color: rgb(216, 0, 0);
+}
+
+.failedLogin::before {
+  content: "!";
+  margin-right: 8px;
+  color: red;
+  font-size: 15px;
   font-weight: bolder;
 }
 </style>
