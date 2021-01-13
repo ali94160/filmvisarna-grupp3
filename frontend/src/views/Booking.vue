716e548a73528fcf546922b6b3cf80ef3529b425
Qiling <professorqtaku@gmail.com>
2021-01-13 13:54:42 +0100

    created confirm div

--- frontend/src/views/Booking.vue
+++ frontend/src/views/Booking.vue
@@ -1,107 +1,108 @@
 <template>
-  <h1>Booking Page</h1>
-  <div class="container">
-    <div class="salon">
+  <div id="#bookingDiv" v-if="!booked">
+    <h1>Booking Page</h1>
+    <div class="container">
+      <div class="salon"></div>
 
-    </div>
+      <div class="info">
+        <span>Platser: </span>
+        <div class="value-button" v-on:click="decreaseValue">-</div>
+        <input type="number" v-model="chosenSeats" max="8" />
+        <div class="value-button" v-on:click="increaseValue">+</div>
 
-    <div class="info" >
-     
-      <span>Platser: </span>
-      <div class="value-button" v-on:click="decreaseValue">-</div>
-      <input type="number" v-model="chosenSeats" max="8">
-      <div class="value-button" v-on:click="increaseValue">+</div>
-
-
-<div class="tickets">
-
-      <BookingInput v-for="seat in chosenSeatsInput" :key="seat" :ticketNumber="seat" @updateTotalPrice="updateTotalPrice" />
-</div>
-      <p>Totala priset: {{ getTotalPrice }}</p>
-
+        <div :class="{tickets: hasThreeTickets}">
+          <BookingInput
+            v-for="seat in chosenSeatsInput"
+            :key="seat"
+            :ticketNumber="seat"
+            @updateTotalPrice="updateTotalPrice"
+          />
+        </div>
+        <p>Totala priset: {{ getTotalPrice }}</p>
+        <button class="btn" v-on:click="changeBooked">Beställ plats(er)</button>
+      </div>
     </div>
   </div>
 
+  <Confirm v-if="booked" :ticketPrices="ticketPrices"/>
 </template>
 
 <script>
-import BookingInput from '../components/BookingInput.vue'
-import ConfirmButton from '../components/ConfirmButton.vue'
+import BookingInput from "../components/BookingInput.vue";
+import Confirm from "../components/Confirm.vue";
 export default {
-  
-  data(){
-    return{
+  data() {
+    return {
       chosenSeats: 2,
-      maxSeats: 8, 
-      ticketPrices:[0, 0, 0, 0, 0, 0, 0, 0],
-    }
+      maxSeats: 8,
+      ticketPrices: [0, 0, 0, 0, 0, 0, 0, 0],
+      booked: false
+    };
   },
 
-  components:{
+  components: {
     BookingInput,
-    ConfirmButton
+    Confirm
   },
 
-
-  methods:{
-    decreaseValue(){
-      if(this.chosenSeats > 1){
+  methods: {
+    decreaseValue() {
+      if (this.chosenSeats > 1) {
         console.log("before", this.chosenSeats);
         this.chosenSeats--;
         console.log("after", this.chosenSeats);
-        this.ticketPrices[this.chosenSeats] = 0
+        this.ticketPrices[this.chosenSeats] = 0;
       }
     },
 
-    increaseValue(){
-      if(this.chosenSeats < this.maxSeats)
-      this.chosenSeats++;
-      console.log(this.ticketPrices)
+    increaseValue() {
+      if (this.chosenSeats < this.maxSeats) this.chosenSeats++;
+      console.log(this.ticketPrices);
     },
 
-    updateTotalPrice(price, ticketNumber){
-   
-      console.log('ticket number is', ticketNumber)
-      this.ticketPrices[ticketNumber-1]=price;
-
-
-
-  }
-
+    updateTotalPrice(price, ticketNumber) {
+      console.log("ticket number is", ticketNumber);
+      this.ticketPrices[ticketNumber - 1] = price;
+    },
+    changeBooked(){
+      this.booked = !this.booked
+    }
   },
 
-  computed:{
-    chosenSeatsInput(){
+  computed: {
+    chosenSeatsInput() {
       let array = [];
-      for(let seat = 1; seat <= this.chosenSeats; seat++){
-        array.push(seat)
+      for (let seat = 1; seat <= this.chosenSeats; seat++) {
+        array.push(seat);
       }
-      return array
+      return array;
     },
-    getTotalPrice(){
-      let sum=0;
-       for(let i=0; i<this.ticketPrices.length; i++){
-         sum+=this.ticketPrices[i]
-       }
-       return sum
+    getTotalPrice() {
+      let sum = 0;
+      for (let i = 0; i < this.ticketPrices.length; i++) {
+        sum += this.ticketPrices[i];
+      }
+      return sum;
+    },
+    hasThreeTickets(){
+      return this.chosenSeats > 3
     }
-  }
-
-}
+  },
+};
 </script>
 
 <style scoped>
-*{
+* {
   margin: 0;
 }
 
 input::-webkit-outer-spin-button,
 input::-webkit-inner-spin-button {
   -webkit-appearance: none;
-  margin: 0;  
+  margin: 0;
 }
 
-.container input{
+.container input {
   background-color: white !important;
   border-radius: 10px;
   max-width: calc(2vh + 2vw);
@@ -110,34 +111,32 @@ input::-webkit-inner-spin-button {
   color: black;
 }
 
-.container{
+.container {
   width: 100%;
   display: grid;
   grid-template-columns: repeat(2, 3fr);
   grid-gap: 60px;
   padding: 30px;
- 
 }
 
-span{
+span {
   padding: 5px;
   color: var(--lightgrey);
 }
 
-.salon, p{
+.salon,
+p {
   background-color: brown;
-  
 }
 
-.salon{
+.salon {
   height: 50vh;
 }
 
-.info{
+.info {
   padding: 10px;
   padding-bottom: 0;
 }
-
 
 .value-button {
   display: inline-block;
@@ -161,15 +160,11 @@ span{
   cursor: pointer;
 }
 
-.tickets{
-   width: 400px;
-  height: 400px;
+.tickets {
+  width: 400px;
+  height: 50vh;
   overflow: scroll;
-  overflow-x:hidden;
+  overflow-x: hidden;
   width: 100%;
 }
-
-
-
-
 </style>
