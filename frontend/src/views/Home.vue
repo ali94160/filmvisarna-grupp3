260605c17ae2b4b7e27c31968c42d824c23c1763
Qiling <professorqtaku@gmail.com>
2021-01-19 09:49:14 +0100

    initialized css carousel

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,22 +1,31 @@
 <template>
 <div class="movieList">
-<MovieItemList />
+  <div class="carousel-wrapper">
+    <CarouselCSS />
+  </div>
+  <MovieItemList />
 </div>
   
 </template>
 
 <script>
 import MovieItemList from "../components/MovieItemList.vue";
+import CarouselCSS from '../components/CasouselCSS.vue';
 export default {
   components: {
     MovieItemList,
+    CarouselCSS
   },
 };
 </script>
 
 <style scoped>
 .movieList{
-  margin-top: 8vw;
+  margin-top: 1vw;
   margin-bottom: 18.2vw; /* For the footer */
+}
+.carousel-wrapper {
+  width: 75vw;
+  margin: 0 auto;
 }
 </style>
