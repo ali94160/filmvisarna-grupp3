015328e05b94b6d67e900998bbdf4df9abf3ac5b
Qiling <professorqtaku@gmail.com>
2021-01-19 17:07:07 +0100

    added harrypotter

---
+++ frontend/src/components/Carousel.vue
@@ -0,0 +1,54 @@
+<template>
+  <div v-if="movies" class="carousel carousel-slider center">
+    <div class="carousel-item red white-text" href="#one!">
+      <img src="../assets/harryPotter.png" alt="">
+    </div>
+    <div class="carousel-item amber white-text" href="#two!">
+      <img src="../assets/harryPotter.png" alt="">
+    </div>
+    <div class="carousel-item green white-text" href="#three!">
+      <img src="../assets/harryPotter.png" alt="">
+    </div>
+    <div class="carousel-item blue white-text" href="#four!">
+      <img src="../assets/harryPotter.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+import M from 'materialize-css'
+
+export default {
+  data(){
+    return{
+      movie: []
+    }
+  },
+  methods:{
+    setMovies(){
+      this.movies = this.$store.state.movies
+    }
+  },
+  computed:{
+    movies(){
+      return this.$store.state.movies
+    }
+  },
+  mounted(){
+    let elems = document.querySelectorAll(".carousel");
+    let instance = M.Carousel.init(elems, {
+    fullWidth: true,
+    indicators: true
+  });
+  }
+}
+</script>
+
+<style>
+.carousel{
+  width: 70% !important;
+  margin: 0 auto;
+  height: 20vh !important;
+}
+
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,19 @@
 <template>
 <div class="movieList">
+<Carousel />
 <MovieItemList />
+
 </div>
   
 </template>
 
 <script>
 import MovieItemList from "../components/MovieItemList.vue";
+import Carousel from "../components/Carousel.vue"
 export default {
   components: {
     MovieItemList,
+    Carousel
   },
 };
 </script>
