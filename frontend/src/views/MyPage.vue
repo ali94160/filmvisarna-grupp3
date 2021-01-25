569937ed0e3c2113e80edf11e10d3171785a1d52
postila <isabella.postila@hotmail.com>
2021-01-25 10:39:33 +0100

    Filter out passed dates in tickets
    Co-Authored-By: Ali <72066097+ali94160@users.noreply.github.com>
    Co-Authored-By: Anna Tch <72065138+anna636@users.noreply.github.com>
    Co-Authored-By: Alexander S. <70532114+autumnjava@users.noreply.github.com>
    Co-Authored-By: lukasbloom93 <72065185+lukasbloom93@users.noreply.github.com>
    Co-Authored-By: oscargregorius <54839993+oscargregorius@users.noreply.github.com>
    Co-Authored-By: Qiling <72065263+professorqtaku@users.noreply.github.com>

--- frontend/src/views/MyPage.vue
+++ frontend/src/views/MyPage.vue
@@ -10,6 +10,7 @@
 
     <div class="past">
       <h4>Historik</h4>
+       <Ticket v-for="ticket of historyTickets" :key="ticket.id" :id="ticket.id"/>
     </div>
   </div>
 </template>
@@ -19,9 +20,23 @@ import Ticket from '../components/Ticket.vue'
 
 
 export default {
+  data(){
+    return {
+      historyTickets: []
+    }
+  },
   computed: {
     getTickets(){
-      return this.$store.state.currentUserTickets;
+      let filteredTickets = []
+      for(let ticket of this.$store.state.currentUserTickets){
+        if((ticket.timeStamp * 1000) > new Date().getTime()){
+          filteredTickets.push(ticket);
+        }
+        else{
+          this.historyTickets.push(ticket);
+        }
+      }
+      return filteredTickets;
     }
   },
   created(){
