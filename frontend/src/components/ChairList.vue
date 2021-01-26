04ea7421b5abb5aced2e5a300bbb3482e70d4619
Ali94160 <ali.al-mousawi@hotmail.com>
2021-01-26 09:45:57 +0100

    Update: booking - moviescreen added to chair div

--- frontend/src/components/ChairList.vue
+++ frontend/src/components/ChairList.vue
@@ -1,5 +1,8 @@
 <template>
-  <h3>{{ salon.name }}</h3>
+  <div class="h3Div">
+    <h3>{{ salon.name }}</h3>
+    <span>FILMDUK</span>
+  </div>
   <div class="salon">
     <div
       v-for="(seatsPerRow, row) of salon.seatsPerRow"
@@ -112,27 +115,33 @@ export default {
     showSelectedSeats() {
       let sc = [];
       for (let s of this.selectedChairs) {
-      sc.push(parseInt('' + s[0] + s[1]))
+        sc.push(parseInt("" + s[0] + s[1]));
       }
       this.$emit("updateSelectedChairs", sc);
       return sc;
     },
   },
   created() {
-    if(this.movie.seatsTaken !== null){
-    if(!this.movie.seatsTaken.includes(34) && !this.movie.seatsTaken.includes(35) ){
+    if (this.movie.seatsTaken !== null) {
+      if (
+        !this.movie.seatsTaken.includes(34) &&
+        !this.movie.seatsTaken.includes(35)
+      ) {
         this.selectedChairs.push([3, 4]);
         this.selectedChairs.push([3, 5]);
-       this.$store.commit("setSelectedSeats", this.selectedChairs.length);
-    }else if(!this.movie.seatsTaken.includes(44) && !this.movie.seatsTaken.includes(45) ){
-      this.selectedChairs.push([4, 4]);
+        this.$store.commit("setSelectedSeats", this.selectedChairs.length);
+      } else if (
+        !this.movie.seatsTaken.includes(44) &&
+        !this.movie.seatsTaken.includes(45)
+      ) {
+        this.selectedChairs.push([4, 4]);
         this.selectedChairs.push([4, 5]);
-       this.$store.commit("setSelectedSeats", this.selectedChairs.length);
-    }
-    }else{
-        this.selectedChairs.push([3, 4]);
-        this.selectedChairs.push([3, 5]);
-       this.$store.commit("setSelectedSeats", this.selectedChairs.length);
+        this.$store.commit("setSelectedSeats", this.selectedChairs.length);
+      }
+    } else {
+      this.selectedChairs.push([3, 4]);
+      this.selectedChairs.push([3, 5]);
+      this.$store.commit("setSelectedSeats", this.selectedChairs.length);
     }
   },
 };
@@ -141,6 +150,17 @@ export default {
 <style scoped>
 * {
   user-select: none;
+}
+.h3Div span {
+  display: block;
+  text-align: center;
+  color: black;
+  background: white;
+  margin: 10px auto;
+  width: 400px;
+}
+.h3Div{
+  margin-bottom: 30px;
 }
 .active {
   color: #4caf50;
@@ -183,9 +203,9 @@ div.highlight {
   cursor: default;
   color: red;
 }
-button{
-   text-align: center;
-    display: block;
+button {
+  text-align: center;
+  display: block;
   background: var(--green);
   text-transform: uppercase;
   font-weight: 20px;
@@ -196,15 +216,14 @@ button{
   border: none;
   width: 80px;
   height: 25px;
-  font-size:14px;
+  font-size: 14px;
   transition: background-color 0.2s ease-out 0s;
-  letter-spacing: 0.8px;     
+  letter-spacing: 0.8px;
   outline: none;
- 
 }
 
-button:hover{
-    background: crimson;
+button:hover {
+  background: crimson;
   cursor: pointer;
 }
 </style>
--- frontend/src/views/MyPage.vue
+++ frontend/src/views/MyPage.vue
@@ -1,63 +1,66 @@
 <template>
- 
   <div class="container">
     <div class="coming">
       <h4>Bokningar</h4>
 
-      <Ticket v-for="ticket of getTickets" :key="ticket.id" :id="ticket.id" :ticket="ticket"/>
+      <Ticket
+        v-for="ticket of getTickets"
+        :key="ticket.id"
+        :id="ticket.id"
+        :ticket="ticket"
+      />
     </div>
-
 
     <div class="past">
       <h4>Historik</h4>
-       <Ticket v-for="ticket of historyTickets" :key="ticket.id" :id="ticket.id"/>
+      <Ticket
+        v-for="ticket of historyTickets"
+        :key="ticket.id"
+        :id="ticket.id"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import Ticket from '../components/Ticket.vue'
-
+import Ticket from "../components/Ticket.vue";
 
 export default {
-  data(){
+  data() {
     return {
-      historyTickets: []
-    }
+      historyTickets: [],
+    };
   },
   computed: {
-    getTickets(){
-      let filteredTickets = []
-      for(let ticket of this.$store.state.currentUserTickets){
-        if((ticket.timeStamp * 1000) > new Date().getTime()){
+    getTickets() {
+      let filteredTickets = [];
+      for (let ticket of this.$store.state.currentUserTickets) {
+        if (ticket.timeStamp * 1000 > new Date().getTime()) {
           filteredTickets.push(ticket);
           filteredTickets.sort((a, b) => {
             return a.timeStamp - b.timeStamp;
-             });
-        }
-        else{
+          });
+        } else {
           this.historyTickets.push(ticket);
         }
       }
       return filteredTickets;
-    }
+    },
   },
-  created(){
-    this.$store.dispatch(
-      'fetchTicketsFromUser', this.$store.state.user.id)
+  created() {
+    this.$store.dispatch("fetchTicketsFromUser", this.$store.state.user.id);
   },
-  components:{
-    Ticket
-  }
-}
+  components: {
+    Ticket,
+  },
+};
 </script>
 
 <style scoped>
-.past{
-background: rgb(29, 29, 29);
-border-radius: 2%;
-margin-left: 10px;
-
+.past {
+  background: rgb(29, 29, 29);
+  border-radius: 2%;
+  margin-left: 10px;
 }
 
 .container {
@@ -71,13 +74,10 @@ h4 {
   margin: 15px auto;
   width: fit-content;
   border-bottom: solid rgba(255, 255, 255, 0.212) 2px;
- 
 }
 
-.coming{
+.coming {
   border-radius: 2%;
   background: rgb(29, 29, 29);
 }
-
-
 </style>
