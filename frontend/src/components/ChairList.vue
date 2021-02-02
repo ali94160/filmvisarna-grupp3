8c58a1dc76ada30fdb3db34ac52df33bfa93dc65
Alexander S <70532114+autumnjava@users.noreply.github.com>
2021-02-02 18:21:26 +0100

    cleaner

--- frontend/src/components/ChairList.vue
+++ frontend/src/components/ChairList.vue
@@ -51,22 +51,16 @@ export default {
       }
       let add = true;
       for (let i = 0; i < this.selectedChairs.length; i++) {
-        for (let j = 0; j < this.selectedChairs.length; j++) {
-          if (
-            this.selectedChairs[i][0] === row &&
-            this.selectedChairs[i][1] === col
-          ) {
+          if (this.selectedChairs[i] === row + '' + col) {
             this.selectedChairs.splice(i, 1);
             this.$store.commit("decreaseSeats");
             this.$emit("decreaseValues");
             add = false;
             return;
-          }
         }
       }
       if (add) {
-
-        this.selectedChairs.push([row, col]);
+        this.selectedChairs.push(row + '' + col);
         this.$store.commit("setSelectedSeatsAmount", this.selectedChairs.length);
       }
     },
@@ -80,14 +74,10 @@ export default {
       this.showSelectedSeats;
       let check = false;
         for (let i = 0; i < this.selectedChairs.length; i++) {
-          for (let j = 0; j < this.selectedChairs.length; j++) {
-            if (
-              this.selectedChairs[i][0] === row &&
-              this.selectedChairs[i][1] === col
-            ) {
+            if (this.selectedChairs[i] === row + '' + col) {
               check = true;
             }
-          }
+          
         }
       
       return check;
